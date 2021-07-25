b995990aabc3e47a941c2de042318c2bdd3402b2
keisuke <keisho@toki.waseda.jp>
2021-07-25 20:34:24 +0900

    added main function

--- kadai.cpp
+++ kadai.cpp
@@ -4,28 +4,40 @@ class Calculator{
     public:
         double a;
         double b;
-        double add();
-        double sub();
-        double mul();
-        double div();
+        double add(double a, double b);
+        double sub(double a, double b);
+        double mul(double a, double b);
+        double div(double a, double b);
 };
 
-double Calculator::add()
+double Calculator::add(double a, double b)
     {
         return a + b;
     }
 
-double Calculator::sub()
+double Calculator::sub(double a, double b)
     {
         return a - b;
     }
 
-double Calculator::mul()
+double Calculator::mul(double a, double b)
     {
         return a * b;
     }
 
-double Calculator::sub()
+double Calculator::div(double a, double b)
     {
         return a / b;
+    }
+
+int main()
+    {
+        Calculator cal;
+
+        std::cout << cal.add(15.0, 3.0) << std::endl;
+        std::cout << cal.sub(15.0, 3.0) << std::endl;
+        std::cout << cal.mul(15.0, 3.0) << std::endl;
+        std::cout << cal.div(15.0, 3.0) << std::endl;
+
+        return 0;
     }
